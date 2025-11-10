eb9586ac8b17d61ace326961da7eb8796c8ff825
Arvin Arbabi <100494430+ArvinArbabi@users.noreply.github.com>
2025-11-10 23:37:05 +0000

    rollback 2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/cmpm-121-f25-d3/",
-  build: {
-    outDir: "docs",
-    emptyOutDir: true,
-  },
+// https://vitejs.dev/config/
+export default {
+  base: Deno.env.get("cmpm-121-f25-d3") || "/project",
   server: {
     port: 3000,
     open: true,
   },
-});
+  build: {
+    target: "baseline-widely-available",
+    outDir: "dist",
+    sourcemap: true,
+  },
+};
