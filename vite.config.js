5a0702780b7147e15c31e57bf73c0d7010963f0b
Arvin Arbabi <100494430+ArvinArbabi@users.noreply.github.com>
2025-11-10 23:28:05 +0000

    attempting to have it run on git pt 2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-// https://vitejs.dev/config/
-export default {
-  base: Deno.env.get("cmpm-121-f25-d3") || "/project",
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/cmpm-121-f25-d3/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
     open: true,
   },
-  build: {
-    target: "baseline-widely-available",
-    outDir: "dist",
-    sourcemap: true,
-  },
-};
+});
